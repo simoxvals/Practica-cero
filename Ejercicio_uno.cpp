3adaf6ea9272777e534fa167d34efd2a8f2404ca
simon <valenciasimon99@gmail.com>
2024-08-19 08:57:26 -0500

    primer commit

---
+++ Ejercicio_uno.cpp
@@ -0,0 +1,9 @@
+//ejercicio para formar por salida un tipo de figura con caracteres; sin solucion trivial
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"startin_test";
+    return 0;
+}
---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Practica00_FILE C:/Users/HP/OneDrive/green/Ingenieria/II/Info II/Practica00/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Practica00.exe)
+set(__QT_DEPLOY_TARGET_Practica00_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Practica00_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
