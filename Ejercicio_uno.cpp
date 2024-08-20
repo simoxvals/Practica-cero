0b85e547ffaa65a8b642c20bc307da483b8b4d06
simon <valenciasimon99@gmail.com>
2024-08-20 12:43:56 -0500

    ejercicio uno realizado

--- Ejercicio_uno.cpp
+++ Ejercicio_uno.cpp
@@ -1,9 +1,34 @@
 //ejercicio para formar por salida un tipo de figura con caracteres; sin solucion trivial
 
 #include <iostream>
+
 using namespace std;
 
-int main(){
-    cout<<"startin_test";
+int main()
+{
+    char letra = 'A';
+
+    for (int i=0; i<7 ; i++){
+        letra = 'A';
+        for(int j=0; j<13; j++){
+            if(j<=i){
+                cout << (letra);
+                letra =letra +1;
+                if((letra)=='H'){
+                    letra=letra-1;
+                }
+                continue;
+            }
+            if(j<12-i){
+                cout << " ";
+                continue;
+            }
+            if (j<13){
+                letra=letra -1;
+                cout << (letra);
+            }
+        }
+        cout << "\n";
+    }
     return 0;
 }
--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,8 +1,16 @@
 //ejercicio dos
-
 #include <iostream>
 using namespace std;
-int main(){
-    cout<<"testing_branch";
+
+int main()
+{
+    char letra = 'A';
+    short int espacios = 11;
+    for (int i = 0; i < 7; i++) {
+        cout << letra;
+        for (short int j = 0; j < espacios; j++) {
+            cout << " "; 
+        }
+    }
     return 0;
 }
